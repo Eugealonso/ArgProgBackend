ae2a192874c1f1e41264316ec81c227f65ebadee
Euge Alonso <alonso.maeuge@gmail.com>
2022-07-24 01:02:43 -0300

    cambios controllers

--- src/main/java/ar/argentinaprograma/portafolio/controller/UsuarioController.java
+++ src/main/java/ar/argentinaprograma/portafolio/controller/UsuarioController.java
@@ -6,7 +6,7 @@ import ar.argentinaprograma.portafolio.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("usuarios")
 public class UsuarioController {
